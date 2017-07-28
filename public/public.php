1503247e640c76acb703a6fa776404a2ab519ab9
Barry Ceelen <b@rryceelen.com>
2017-07-28 19:23:26 +0200

    Add filter for determining when images should be 100% wide

--- public/public.php
+++ public/public.php
@@ -34,5 +34,21 @@ function relative_width_images_enqueue_styles() {
 			'1.0.0',
 			'all'
 		);
+
+		/**
+		 * Filters whether to add an inline style which makes the images 100% wide.
+		 *
+		 * @since 1.0.1
+		 *
+		 * @param bool|string $breakpoint Breakpoint up to where the images should be 100% wide, default false.
+		 */
+		$breakpoint = apply_filters( 'relative_width_images_breakpoint', false );
+
+		if ( $breakpoint ) {
+			wp_add_inline_style(
+				'relative-width-images',
+				'@media screen and (max-width: ' . esc_attr( $breakpoint ) . '){[data-rwi]{width:100%;}}'
+			);
+		}
 	}
 }
